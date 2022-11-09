654e9e36515ee46e4040e464e066deb54b213092
Afolabi Abayomi <104542173+afootechglobal@users.noreply.github.com>
2022-11-09 04:55:22 +0100

    Add files via upload

---
+++ student-transfer.php
@@ -0,0 +1,121 @@
+
+<?php require_once 'config/connection.php' ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<?php include 'links.php'?>
+<!-------------------------------------------------------------->
+<title>Student Transfer Program - <?php echo $thename?></title>
+    <meta name="keywords" content="Student Transfer Programs, Transfer Medical Programs, Student Transfer Medical Programs In Nigeria, Study Abroad, Study Abroad Opportunities, Study Abroad Program, Studying Abroad" />
+    <meta name="description" content="You want to Study from one country to other Country? Student Transfer Medical Program is for prospective students who transfer from another medical school. "/>
+
+    <meta property="og:title" content="Student Transfer Program - <?php echo $thename?>" />
+    <meta property="og:image" content="<?php echo $website?>/image/seo_image/student-transfer-program.jpg"/>
+    <meta property="og:description" content="You want to Study from one country to other Country? Student Transfer Medical Program is for prospective students who transfer from another medical school."/>
+
+    <meta name="twitter:title" content="Student Transfer Program - <?php echo $thename?>"/> 
+    <meta name="twitter:card" content="<?php echo $thename?>"/> 
+    <meta name="twitter:image"  content="<?php echo $website?>/image/seo_image/student-transfer-program.jpg"/> 
+    <meta name="twitter:description" content="You want to Study from one country to other Country? Student Transfer Medical Program is for prospective students who transfer from another medical school."/>
+</head>
+<body>
+<?php include 'header.php'?>
+
+<div class="body-div">
+        <div class="background_div" class="aos-animate" data-aos="fade-down" data-aos-duration="1500">
+            <div class="over_lay"> 
+                <div class="text">
+                    <h1>Student Transfer Program</h1>
+                    <p>Home / <span>CMI Program</span> / <span>Student Transfer Program</span></p>
+                </div>
+            </div>
+        </div>
+
+  
+    
+    <div class="side_back_div">
+        <div class="div_in">
+            <div class="side">
+                    <div class="img_div" class="aos-animate" data-aos="fade-up" data-aos-duration="1500">
+                    <img src="image/student-transfer.jpg" alt="Student Transfer"> 
+                    </div>
+                    <div class="img_div link_div" data-aos="fade-up" data-aos-duration="1000">
+                        <div class="header_link"><i class="fa fa-link"></i>  <h2>Hot Links</h2> </div>
+                                <div class="li">
+                                    <li class="active" title="C.M.I Programs">C.M.I Programs</li>
+                                    <li title="Why CMI">Curriculum</li>
+                                    <li title="CMI Services">Research</li>
+                                    <li title="Accreditation & Approval">International Affiliation</li>
+                                    <li title="Clinical">Financial Service</li>
+                                </div>
+                        </div>
+                    </div>
+                    <div class="side right_side">
+                        <h2>STUDENT TRANSFER PROGRAM</h2>
+
+                            <hr style="border:2px #ccc solid; width:40%;margin-left:0px">
+                            <strong>The Student Transfer Medical Program </strong> is for prospective students who transfer from another medical school. Further information or requirement is provided below:   <br>
+                            Our Admissions committee will look into a variety of factors for an applicant to be considered for admission into the <strong>Student Transfer Medical Program</strong> at one of our affiliated Universities in Nigeria. Some of those are:
+                            </p>
+                            <p><i class="fa fa-dot-circle-o" ></i> <span>Student Transfer Medical Program requirements</span> </p>
+                            <span class="subject"><i class="fa fa-check"></i> SSCE Result, includes: </span><br>
+                        
+                            <span style="padding-left:30px" class="subject"><i class="fa fa-dot-circle-o"></i> Mathematics: Preferably Calculus or Statistics</span><br>
+                            <span style="padding-left:30px" class="subject"><i class="fa fa-dot-circle-o"></i> English</span><br>
+                            <span style="padding-left:30px" class="subject"><i class="fa fa-dot-circle-o"></i> Chemistry</span><br>
+                            <span style="padding-left:30px" class="subject"><i class="fa fa-dot-circle-o"></i> Physics</span><br>
+                            <span style="padding-left:30px" class="subject"><i class="fa fa-dot-circle-o"></i> Biology</span><br>
+
+                            <span class="subject"><i class="fa fa-check"></i> Institution Transcript</span><br>
+                            <span class="subject"><i class="fa fa-check"></i> Two (2) Letter of Recommendation</span><br>
+                            <span class="subject"><i class="fa fa-check"></i> Personal Essay</span><br>
+                            <span class="subject"><i class="fa fa-check"></i> Overview of Activities and Achievememts</span><br>
+                            <span class="subject"><i class="fa fa-check"></i> Personal Interview</span><br>
+                     
+                            <h2><i class="fa fa-dot-circle-o" ></i> Transfer From Another Medical School</h2>
+                        <p>
+                        Transfer Students are accommodated from MD 2 to MD 6 semester and eligibility is determined during the admission process by reviewing the available resources in all learning sectors of the medical education program. Transfer students are accepted after a careful review of their academic performance in their previous medical school which must be listed in the World Directory of Medical Schools. 
+                        The Office of Student Affairs and Admissions will participate in determining the eligibility of candidates applying for transfer. 
+                        The admission committee also seeks scores of the NBME Comprehensive Exam, and USMLE scores if applicable. (This is applicable to students seeking transfer to MD 6 and beyond)
+                        </p>
+                        <a href="login"><div class="btn" title="Apply Now">Apply Now <i class="fa fa-check"></i></div></a>
+                        </div>
+
+
+
+    </div>
+
+    </div>
+
+
+</div>
+
+
+
+<?php include 'footer.php' ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+<script>
+    AOS.init({
+    easing: 'ease-in-out-sine'
+    });
+</script>
